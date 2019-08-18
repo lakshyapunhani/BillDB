2cca8312600f6d3d46f60eb2b4eba0d205d29eb7
Lakshya Punhani <lakshya.punhani@gmail.com>
2019-08-19 00:17:25 +0530

    Delete new db for single user

--- BillStore2/src/main/java/in/fabuleux/billStore2/repos/InvoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package in.fabuleux.billStore2.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.fabuleux.billStore2.entities.Invoice;
-
-public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-
-}
